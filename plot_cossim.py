e33259f9f6f984b94ee6ee0a412410233fed1c23
Viacheslav Surkov <surkov@izar1.izar.cluster>
2024-05-19 16:42:44 +0200

    plot_cossim

--- plot_cossim.py
+++ plot_cossim.py
@@ -9,10 +9,21 @@ rare_enc = encoder.W_enc[:, is_rare]
 rare_mean = rare_enc.mean(-1)
 is_norm = (log_freqs > -2.5)
 
+plt.rcParams['font.size'] = 16
+print(log_freqs.shape)
+
+plt.figure(figsize=(8, 6))
+plt.hist(log_freqs, bins=30)
+plt.xlabel('Log frequencies')
+plt.grid()
+plt.savefig('../out.png')
+
+plt.figure(figsize=(8, 6))
 cos_sim = rare_mean @ encoder.W_enc / rare_mean.norm() / encoder.W_enc.norm(dim=0)
 cos_sim = cos_sim.detach().cpu().numpy()
 plt.hist(cos_sim[is_rare], label='Rare', bins=30)
 plt.hist(cos_sim[is_norm], label='Norm', bins=30)
 plt.grid()
 plt.legend()
+plt.xlabel('Cosine similarity')
 plt.savefig('../cossim.png')
